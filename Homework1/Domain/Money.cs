5d97d2c069638d2231634f7a0fee3fcb02fab79e
Alexey Goncharov <cianis2002@gmail.com>
2023-07-16 23:49:29 +0300

    Added operators and methods to Money

--- Homework1/Domain/Money.cs
+++ Homework1/Domain/Money.cs
@@ -5,6 +5,9 @@
 /// </summary>
 public class Money
 {
+    private const int RublesToKopeksFactor = 100;
+    private const string MinusSign = "-";
+
     public Money(int rubles, int kopeks)
         : this(false, rubles, kopeks)
     {
@@ -31,4 +34,95 @@ public class Money
     /// Количество копеек
     /// </summary>
     public int Kopeks { get; }
+
+    public static Money operator +(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        int resultTotalKopeks = leftTotalKopeks + rightTotalKopeks;
+
+        return CreateMoneyFromTotalKopeks(resultTotalKopeks);
+    }
+
+    public static Money operator -(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        int resultTotalKopeks = leftTotalKopeks - rightTotalKopeks;
+
+        return CreateMoneyFromTotalKopeks(resultTotalKopeks);
+    }
+
+    public static bool operator >(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        return leftTotalKopeks > rightTotalKopeks;
+    }
+
+    public static bool operator <(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        return leftTotalKopeks < rightTotalKopeks;
+    }
+
+    public static bool operator >=(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        return leftTotalKopeks >= rightTotalKopeks;
+    }
+
+    public static bool operator <=(Money left, Money right)
+    {
+        int leftTotalKopeks = left.CalculateTotalKopeks();
+        int rightTotalKopeks = right.CalculateTotalKopeks();
+
+        return leftTotalKopeks <= rightTotalKopeks;
+    }
+
+    public override string ToString()
+    {
+        string sign = string.Empty;
+        if (IsNegative == true)
+        {
+            sign = MinusSign;
+        }
+
+        return $"Account balance: {sign}{Rubles.ToString()}{Kopeks.ToString()}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Money other
+               && IsNegative == other.IsNegative
+               && Rubles == other.Rubles
+               && Kopeks == other.Kopeks;
+    }
+
+    public override int GetHashCode()
+    {
+        return CalculateTotalKopeks();
+    }
+
+    private static Money CreateMoneyFromTotalKopeks(int totalKopeks)
+    {
+        int rubles = totalKopeks / RublesToKopeksFactor;
+        int kopeks = totalKopeks % RublesToKopeksFactor;
+        bool isNegative = totalKopeks < 0;
+
+        return new Money(isNegative: isNegative, rubles, kopeks);
+    }
+
+    private int CalculateTotalKopeks()
+    {
+        int sign = IsNegative ? -1 : 1;
+        return sign * ((Rubles * RublesToKopeksFactor) + Kopeks);
+    }
 }
