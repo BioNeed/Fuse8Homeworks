8624ee017123505d52f0f154404c579be324b6ba
Alexey Goncharov <cianis2002@gmail.com>
2023-07-22 15:37:58 +0300

    Added implementation of Lazy

--- Homework2/Domain/Lazy.cs
+++ Homework2/Domain/Lazy.cs
@@ -5,7 +5,28 @@
 /// </summary>
 public class Lazy<TValue>
 {
-	// TODO Реализовать ленивое получение значение при первом обращении к Value
+    private TValue? value;
+    private Func<TValue> acion;
+    private bool isInitializedProperty;
 
-	public TValue? Value { get; }
+    public Lazy(Func<TValue> func)
+    {
+        acion = func;
+    }
+
+    // TODO Реализовать ленивое получение значение при первом обращении к Value
+
+    public TValue? Value 
+    {
+        get
+        {
+            if (isInitializedProperty == false)
+            {
+                value = acion();
+                isInitializedProperty = true;
+            }
+
+            return value;
+        }
+    }
 }
