d84dfbfc8d87e17203a296d9ff5823359138bff7
Alexey Goncharov <cianis2002@gmail.com>
2023-08-07 13:51:08 +0300

    Added PetProject

---
+++ PetProject/CurrencyApi/PublicApi/Exceptions/InvalidDateFormatException.cs
@@ -0,0 +1,10 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions
+{
+    public class InvalidDateFormatException : Exception
+    {
+        public InvalidDateFormatException(string message = "")
+            : base(message)
+        {
+        }
+    }
+}
